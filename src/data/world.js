e22bfc4e774ffad6f6b564935dc0a3d514b7d1bf
ElissonDeSous <94570280+ElissonDeSous@users.noreply.github.com>
2023-05-07 20:23:11 -0300

    Add files via upload

--- src/data/world.js
+++ src/data/world.js
@@ -5,6 +5,11 @@ export const wordsList = {
     computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
     programação: ["Linguagem", "Framework", "JavaScript", "React"],
     alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+    filmes: ["HarryPotter", "Click", "Vingadores", "Shrek", "Titanic"],
+    series: ["Supernatural", "breakingBad", "Dark", "Arcane", ""],
+    pais: ["Brasil", "canada", "Portugal", "China", "Japão"],
+    TimesDeFutebol: ["SãoPaulo", "Palmeiras", "Corinthians", "Flamengo", "Santos"],
+    anime: ["OnePiece", "Naruto", "DemonSlayer", "FairyTail", "BokuNoHero"],
 
 
 
