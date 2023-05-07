fea58a7155bc7afc23d1d5c7086d9f1af513a873
ElissonDeSous <94570280+ElissonDeSous@users.noreply.github.com>
2023-05-07 17:03:02 -0300

    Add files via upload

--- src/componentes/GamesOver.js
+++ src/componentes/GamesOver.js
@@ -2,7 +2,7 @@ import './gamerOver.css'
 const GamesOver = ({Reiniciar,Score}) => {
   return (
     <div>
-      <h1>GamesOver</h1>
+      <h1>Fim do Jogo</h1>
       <h2>sua pontuação foi : <span>{Score}</span> </h2>
       <button onClick={Reiniciar} >Reiniciar</button>
     </div>
